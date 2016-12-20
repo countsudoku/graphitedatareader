c05db6d25bb4d1333fb510ed54bf6a623e4d32a3
Moritz Schneider <schneider.moritz@gmail.com>
2016-12-20 18:17:20 +0100

    Added a basic setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name='graphitedatareader',
+    packages=['graphitedatareader',],
+    version='0.1.0',
+    description='Read data from GRaphite into a pandas DataFrame or Panel',
+    author='Moritz C.K.U. Schneider',
+    author_email='schneider.moritz@gmail.com',
+    #url='https://github.com/XXX',
+    keywords=['graphite' ],
+    license='BSD License',
+    install_requires=['pandas', 'requests'],
+)
